156aa9374c1348bdae1aedc0e2692b59b0df22e6
Tom <thomasastewart99@gmail.com>
2022-12-18 21:01:27 +0000

    added get current location helper

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: const SizedBox()),
+      body: SafeArea(child: FutureBuilder()),
     );
   }
 }
